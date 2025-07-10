5af7bf65bbbd9d324ead7b9a7275b1f2827896a0
PemaDechen <padenlaa@gmail.com>
2025-07-10 09:57:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
+import { AgGridReact } from 'ag-grid-react';
+import 'ag-grid-community/styles/ag-grid.css'
+import 'ag-grid-community/styles/ag-theme-alpine.css'
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [columnDefs] = useState([
+    {  field: "make" },
+    {  field: "model" },
+    {  field: "price" },
+  ]);
+  const [rowDefs] = useState([
+    {  make:"Toyota", model:"Celecia", price: 3500000 },
+    {  make:"Ford", model:"Mondeo", price: 3200000 },
+    {  make:"Porche", model:"Boxter", price: 750000 },
+  ]);
+  return ;
 }
 
 export default App;
