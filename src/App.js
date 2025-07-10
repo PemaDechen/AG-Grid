d63ebeeeb8b44f3b25208b75cb032969661065aa
PemaDechen <padenlaa@gmail.com>
2025-07-10 10:07:56 +0530

    imported AG grid correctly it seems there are two ways of importing one is package based another is module based, in module we have to register modules

--- src/App.js
+++ src/App.js
@@ -1,21 +1,29 @@
-import React, {useState} from 'react';
+import React , { useState } from 'react';
+import { ModuleRegistry, AllCommunityModule } from 'ag-grid-community';
 import { AgGridReact } from 'ag-grid-react';
-import 'ag-grid-community/styles/ag-grid.css'
-import 'ag-grid-community/styles/ag-theme-alpine.css'
-import './App.css';
+
+import 'ag-grid-community/styles/ag-grid.css';
+import 'ag-grid-community/styles/ag-theme-alpine.css';
+
+
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 function App() {
   const [columnDefs] = useState([
-    {  field: "make" },
-    {  field: "model" },
-    {  field: "price" },
+    { field: "make" },
+    { field: "model" },
+    { field: "price" },
   ]);
-  const [rowDefs] = useState([
-    {  make:"Toyota", model:"Celecia", price: 3500000 },
-    {  make:"Ford", model:"Mondeo", price: 3200000 },
-    {  make:"Porche", model:"Boxter", price: 750000 },
+  const [rowData] = useState([
+    { make: "Toyota", model: "Celecia", price: 3500000 },
+    { make: "Ford", model: "Mondeo", price: 3200000 },
+    { make: "Porche", model: "Boxter`", price: 750000 },
   ]);
-  return ;
+  return (
+    <div className="ag-theme-alpine" style={{ height: 400, width: 600 }}>
+      <AgGridReact rowData={rowData} columnDefs={columnDefs} />
+    </div>
+  );
 }
 
 export default App;
